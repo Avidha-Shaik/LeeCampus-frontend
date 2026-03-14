686269487782994d1413b943fb6a889dbb171b4b
Avidha Shaik <avidha@gmail.com>
2026-03-14 17:58:58 +0530

    added render url in axiosconfigjs

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/leecampus",
+  baseURL: "https://leecampus-backend.onrender.com/leecampus",
 });
 
 export default api;
